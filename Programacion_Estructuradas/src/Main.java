0ec1d67752073ea7f7ea1cc1d883b1742a0c5028
FlorGubiotti <florgubiotti@gmail.com>
2026-03-06 10:56:32 -0300

    programacion estructurada

--- Programacion_Estructuradas/src/Main.java
+++ Programacion_Estructuradas/src/Main.java
@@ -1,41 +1,40 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double[] notas = new double[3];
+        Scanner sc = new Scanner(System.in);
 
-        ArrayList<Double> notas = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-
-        //pedir las 3 notas
-        for (int i=0;i <3; i++) {
+        // pedir las 3 notas
+        for (int i = 0; i < 3; i++) {
             System.out.println("Ingrese la nota " + (i + 1) + ":");
-            double nota = Double.parseDouble(scanner.nextLine());
-            notas.add(nota);
+            notas[i] = Double.parseDouble(sc.nextLine());
         }
-        //mostrar resultados
-        System.out.println("Promedio:"+calcularPromedio(notas));
-        System.out.println("Mayor:"+ calcularMaximo(notas));
 
+        // mostrar resultados
+        System.out.println("Promedio: " + calcularPromedio(notas));
+        System.out.println("Mayor: " + calcularMaximo(notas));
+
+        sc.close();
     }
 
-    //calcular promedio
-    public static double calcularPromedio(ArrayList<Double> lista) {
-    double suma=0;
-    for (int i=0;i<lista.size();i++) {
-        suma +=lista.get(i);
-    }
-    return suma/lista.size();
-    }
-    public static double calcularMaximo(ArrayList<Double> lista) {
-    double mayor=0;
-    for (int i=1;i<lista.size();i++) {
-        if(lista.get(i)>mayor){
-            mayor=lista.get(i);
+    // calcular promedio
+    public static double calcularPromedio(double[] lista) {
+        double suma = 0;
+        for (int i = 0; i < lista.length; i++) {
+            suma += lista[i];
         }
-    }
-    return mayor;
+        return suma / lista.length;
     }
 
-
+    // calcular máximo
+    public static double calcularMaximo(double[] lista) {
+        double mayor = lista[0];
+        for (int i = 1; i < lista.length; i++) {
+            if (lista[i] > mayor) {
+                mayor = lista[i];
+            }
+        }
+        return mayor;
+    }
 }
